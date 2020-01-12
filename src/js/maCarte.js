fdd8fd4d8951cc6319f8d21cb00c7d4645ad291e
Thierry <contact62000@gmail.com>
2020-01-12 10:11:02 +0100

    update

--- src/js/maCarte.js
+++ src/js/maCarte.js
@@ -13,7 +13,6 @@ function initMap() {
     // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
     macarte = L.map('map').setView([latAgence, lonAgence], 14);
 
-    
     // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         // Il est toujours bien de laisser le lien vers la source des données

caf684e9f1625748ff1c0c4fea7951a9a088fa71
Thierry BROUET <56256915+thierryDev62@users.noreply.github.com>
2021-07-03 19:21:00 +0200

    update

--- src/js/map.js
+++ src/js/map.js
@@ -1,3 +1,9 @@
+/*
+* Brouet Thierry
+*
+* 2021
+*  */
+
 let lat = 50.4676822;
 let lng = 2.5603700000000345;
 let map = null;
@@ -5,15 +11,16 @@ let map = null;
 // Map initialization
 function initMap() {
 
-    map = L.map('map').setView([lat, lng], 14);
+    map = L.map('map').setView([lat, lng], 15);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>&nbsp&nbsp',
         minZoom: 10,
-        maxZoom: 18
-    }).addTo(map);
+        maxZoom: 18,
+    })
+        .addTo(map);
 
-    // Marker
+    // Custom Marker
     let icon = L.icon({
         iconUrl: 'src/img/icon.png',
         iconSize: [65, 65],
@@ -21,10 +28,12 @@ function initMap() {
         popupAnchor: [-1, -60]
     });
 
-    let marker = L.marker([lat, lng], {icon: icon}).addTo(map);
+    let marker = L.marker([lat, lng], {icon: icon})
+        .addTo(map);
 
     marker.addEventListener("mouseover", () => {
-        marker.bindPopup("<h3>Marker test</h3><br>Here is the text of marker test").openPopup();
+        marker.bindPopup("<h3>Marker test</h3><br>Here is the text of marker test")
+            .openPopup();
     });
 
     marker.addEventListener("mouseout", () => {
